35725546c87d5365821841c555f6e7d4257542c7
shpakb <shpakb@gmail.com>
2019-12-23 05:36:22 -0600

    fix

--- scripts/R/gsm_to_gse.R
+++ scripts/R/gsm_to_gse.R
@@ -2,18 +2,16 @@ suppressMessages(library(tidyverse))
 
 args <- commandArgs(TRUE)
 
-gse_file <- args[1]
+
+gene_anotation_file <- args[1]
+gse_file <- args[2]
 gse_id <- gse_file %>% str_extract("GSE\\d+")
-gene_anotation_file <- args[2]
 gsm_files <- args[3:length(args)]
 
 cat(sprintf("Working on: %s \n", gse_id))
 cat(sprintf("Output file: %s \n", gse_file))
 cat(sprintf("Gene annotation: %s \n", gene_anotation_file))
 cat(sprintf("Aggregating %i GSM \n", length(gsm_files)))
-
-print(args)
-print(gsm_files)
 
 # getting Ensamble to Entrez mapping
 # removing all genes without Entrez annotation
@@ -101,14 +99,13 @@ aggregate_gse <- function(gsm_files, geneAnnot) {
 
 # getting filtered list of gsm for GSE
 # filtering list by QC
-
 gse <- aggregate_gse(gsm_files, geneAnnot)
 
-# rounding for memory efficiency
+# rounding for compression
 gse[, unlist(lapply(gse, is.numeric))] <-
   round(gse[, unlist(lapply(gse, is.numeric))], 3)
 
-# write GSE
+print("Writing expression table...")
 write.table(x=gse,
             gse_file,
             sep = "\t",
