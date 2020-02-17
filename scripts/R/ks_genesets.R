16b90305c06e5661c1ea05975d1c709ac3948db8
shpakb <shpakb@gmail.com>
2020-02-17 16:41:28 -0600

    fix

--- scripts/R/ks_genesets.R
+++ scripts/R/ks_genesets.R
@@ -30,7 +30,7 @@ for (pc_name in names(pc_list)){
     print(pc_name)
     pc <- pc_list[[pc_name]]
     overlap <- pc[geneset]
-    overlap <- overlap[!is.na(name(overlap))]
+    overlap <- overlap[!is.na(names(overlap))]
     ks_result <- ks.test(pc, overlap)
     ks_result <- ks_result %>% unlist %>% t %>% as.data.frame %>% select("statistic.D", "p.value")
     ks_result$LABEL <- pc_name
